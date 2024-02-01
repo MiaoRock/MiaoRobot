25b9ea7fc90336d2db7915f7e18d6aa8edf79ea5
MiaoRock <mh281295900@163.com>
2024-02-01 14:35:08 +0800

    fixed the client

--- DingRobotClient/src/main/java/com/miao/robot/ding/client/DingMiaoClient.java
+++ DingRobotClient/src/main/java/com/miao/robot/ding/client/DingMiaoClient.java
@@ -7,10 +7,12 @@ import com.dingtalk.api.response.OapiRobotSendResponse;
 import com.taobao.api.ApiException;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 @Slf4j
 public class DingMiaoClient {
 
-    public static String access = "346b9182eaa24e76e2d0ad3039b1395fa4b4653c5c6b2b35a0289d0d5a27eb73";
+    public static String access = "XXXX";
 
     public static void main(String[] args) throws ApiException {
         String serverUrl = "https://oapi.dingtalk.com/robot/send?access_token=" + DingMiaoClient.access;
@@ -22,8 +24,8 @@ public class DingMiaoClient {
         text.setContent("测试一下哦，别在意~");
         request.setText(text);
         OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
-//        at.setAtMobiles(Arrays.asList("+86-18640093452"));
-//        at.setIsAtAll(true);
+//        at.setAtMobiles(Arrays.asList("+86-XXXXXXXXXXX"));
+//        at.setIsAtAll(false);
         request.setAt(at);
 
         OapiRobotSendResponse response = client.execute(request);
